9d51595bb397782d234317221001f7772c999024
YevhenTar <58787169+YevhenTar@users.noreply.github.com>
2022-11-19 00:38:19 +0200

    Change API settings

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -9,13 +9,13 @@ import { forkJoin } from 'rxjs';
 export class HttpService {
 
   constructor(private http: HttpClient) { }
-
+  // 6b3ea2c0ca5183b4fd02
 
   getData(){
     return forkJoin([
-    this.http.get('https://free.currconv.com/api/v7/convert?q=UAH_USD,UAH_EUR&compact=ultra&apiKey=6b3ea2c0ca5183b4fd02'),
-    this.http.get('https://free.currconv.com/api/v7/convert?q=USD_EUR,USD_UAH&compact=ultra&apiKey=6b3ea2c0ca5183b4fd02'),
-    this.http.get('https://free.currconv.com/api/v7/convert?q=EUR_UAH,EUR_USD&compact=ultra&apiKey=6b3ea2c0ca5183b4fd02')
+    this.http.get('https://free.currconv.com/api/v7/convert?q=UAH_USD,UAH_EUR&compact=ultra&apiKey=ce0ce263e3fb05abf982'),
+    this.http.get('https://free.currconv.com/api/v7/convert?q=USD_EUR,USD_UAH&compact=ultra&apiKey=ce0ce263e3fb05abf982'),
+    this.http.get('https://free.currconv.com/api/v7/convert?q=EUR_UAH,EUR_USD&compact=ultra&apiKey=ce0ce263e3fb05abf982')
     ]);
 }
 }
