01b4c4e9510afaa962299f2b9889a0a15ab81ad7
YevhenTar <yevhen.tar@gmail.com>
2023-04-22 17:37:05 +0300

    Upgrade API code for April

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -12,9 +12,9 @@ export class HttpService {
 
   getData(){
     return forkJoin([
-    this.http.get('https://free.currconv.com/api/v7/convert?q=UAH_USD,UAH_EUR&compact=ultra&apiKey=ce0ce263e3fb05abf982'),
-    this.http.get('https://free.currconv.com/api/v7/convert?q=USD_EUR,USD_UAH&compact=ultra&apiKey=ce0ce263e3fb05abf982'),
-    this.http.get('https://free.currconv.com/api/v7/convert?q=EUR_UAH,EUR_USD&compact=ultra&apiKey=ce0ce263e3fb05abf982')
+    this.http.get('https://free.currconv.com/api/v7/convert?q=UAH_USD,UAH_EUR&compact=ultra&apiKey=7747e155eb6ee0399d06'),
+    this.http.get('https://free.currconv.com/api/v7/convert?q=USD_EUR,USD_UAH&compact=ultra&apiKey=7747e155eb6ee0399d06'),
+    this.http.get('https://free.currconv.com/api/v7/convert?q=EUR_UAH,EUR_USD&compact=ultra&apiKey=7747e155eb6ee0399d06')
     ]);
 }
 }
